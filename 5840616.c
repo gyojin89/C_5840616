6a45f62e69670a9a5a8a67773107b2a67a765e78
5840616 Jeong gyojin <gyojin123456@gmail.com>
2026-03-12 14:30:48 +0900

    Add files via upload

--- 5840616.c
+++ 5840616.c
@@ -1,21 +1,26 @@
-    #include <stdio.h>
-    #include <stdint.h>
+#include <stdio.h>
+#include <stdint.h>   
 
-    int main() {
+int main() {
+   
+    int8_t val = -1;
+    
+    // val의 메모리 주소를 unsigned char 포인터로 변환하는 과정입니다.
+    // 메모리에 저장된 실제 1바이트 값을 확인하기 위해 사용합니다.
+    unsigned char *ptr = (unsigned char *)&val;
 
-        int8_t val = -1;
-        
-        unsigned char *ptr = (unsigned char *)&val;
+    // val의 메모리 값을 16진수 형태로 출력하기 위한 코드입니다.
+    printf("정수 -1의 메모리 값 (16진수): 0x%02X\n", *ptr);
 
-        printf("정수 -1의 메모리 값 (16진수): 0x%02X\n", *ptr);
-
-        if (*ptr == 0xFF) {
-            printf("결과: 현재 사용하고 있는 컴퓨터는 2의 보수 방식을 이용해 정수를 표현하고 있습니다.\n");
-        } else if (*ptr == 0xFE) {
-            printf("결과: 현재 사용하고 있는 컴퓨터는 1의 보수 방식을 이용해 정수를 표현하고 있습니다.\n");
-        } else
-
-        
-
-        return 0;
+    // 데이터가 저장된 메모리의 주소값을 저장하는 변수인 포인트, 즉 *ptr를 이용해 값이 0xFF이면 2의 보수 방식이라고 화면에 나옵니다.
+    if (*ptr == 0xFF) {
+        printf("결과: 현재 사용하고 있는 컴퓨터는 2의 보수 방식을 이용해 정수를 표현하고 있습니다.\n");
     }
+    // 위 코드와 동일하게 *ptr를 이용해 값이 0xFE이면 1의 보수 방식이라고 화면에 나옵니다.
+    else if (*ptr == 0xFE) {
+        printf("결과: 현재 사용하고 있는 컴퓨터는 1의 보수 방식을 이용해 정수를 표현하고 있습니다.\n");
+    }
+
+
+    return 0;
+}
