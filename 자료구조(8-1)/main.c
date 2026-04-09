f82ad7cea61b492dc132feea81da7bbf336e0090
5840616 Jeong gyojin <gyojin123456@gmail.com>
2026-04-09 14:21:07 +0900

    Add files via upload

---
+++ 자료구조(8-1)/main.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+long long fib_recursive(int n) {
+    if (n == 1 || n == 2)
+        return 1;
+    return fib_recursive(n - 1) + fib_recursive(n - 2);
+}
+
+long long fib_iterative(int n) {
+    if (n == 1 || n == 2)
+        return 1;
+
+    long long a = 1, b = 1, result;
+
+    for (int i = 3; i <= n; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return b;
+}
+
+int main() {
+    int N;
+
+    printf("N 입력: ");
+    scanf("%d", &N);
+
+    printf("\nN\t재귀시간\t반복시간\n");
+
+    for (int i = 1; i <= N; i++) {
+        clock_t start, end;
+
+        start = clock();
+        fib_recursive(i);
+        end = clock();
+        double time_recursive = (double)(end - start) / CLOCKS_PER_SEC;
+
+        start = clock();
+        fib_iterative(i);
+        end = clock();
+        double time_iterative = (double)(end - start) / CLOCKS_PER_SEC;
+
+        printf("%d\t%lf\t%lf\n", i, time_recursive, time_iterative);
+    }
+
+    return 0;
+}
