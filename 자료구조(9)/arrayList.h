7dcc886cef0fe0c7ab1b99f85326173773e8d7d8
5840616 Jeong gyojin <gyojin123456@gmail.com>
2026-04-16 14:46:55 +0900

    Add files via upload

---
+++ 자료구조(9)/arrayList.h
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int elementArrayList; 
+
+typedef struct arrayListType {
+    elementArrayList* data; 
+    int capacity;
+    int size;
+} arrayList;
+
+extern arrayList* createArrayList (int size);
+extern void destroyArrayList (arrayList* al);
+extern int isEmptyArrayList (arrayList* al);
+extern int isFullArrayList (arrayList* al);
+extern int sizeArrayList (arrayList* al);
+extern int insertArrayList (arrayList* al,
+    int pos, elementArrayList item);
+extern elementArrayList deleteArrayList(
+    arrayList* al, int pos);
+extern int initArrayList (arrayList* al);
+extern elementArrayList getItemArrayList(
+    arrayList* al, int pos);
+extern int replaceItemArrayList(arrayList* al,
+    int pos, elementArrayList item);
+extern void printArrayList(arrayList* al);
